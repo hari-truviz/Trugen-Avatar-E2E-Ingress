c93d6e91559aaa56e101de8f310cd9e5f7b6f86b
hari-truviz <hari@truviz.ai>
2025-10-17 14:20:33 +0530

    Added support to post Job Status back to connected client.

--- main.go
+++ main.go
@@ -111,6 +111,8 @@ func handleConnection(conn *websocket.Conn, rq *CustomTypes.RequestQueue) {
 				fmt.Println(err)
 				return
 			}
+		} else {
+			conn.Close()
 		}
 	}
 }
@@ -152,7 +154,7 @@ func main() {
 	}
 
 	// Hashmap to keep track of posted job id and web socket connection to notify
-	// postedJobs := make(map[string]CustomTypes.Request)
+	postedJobs := make(map[string]CustomTypes.Request)
 	// Start a ticker to check the Runpod if it's ready
 	ticker := time.NewTicker(1 * time.Second)
 	go func() {
@@ -168,28 +170,31 @@ func main() {
 						fmt.Printf("Unable to post job: %s", err.Error())
 					}
 					fmt.Printf("New job posted with ID: %s and Status: %s\n", response.ID, response.Status)
-					// postedJobs[response.ID] = req
+					postedJobs[response.ID] = req
 				}
 				fmt.Printf("Number of requests in the queue: %v\n", rq.Queue.Len())
 			}
 			// Check the status of the posted jobs and notify on error if required
-			// for id, job := range postedJobs {
-			// 	// Check the status of job in Runpod
-			// 	err, jobStatus := Utilities.GetJobStatus(id)
-			// 	// Drop the item from the map if not IN_QUEUE
-			// 	if err != nil {
-			// 		fmt.Println(err.Error())
-			// 	}
-			// 	// If status is not IN_QUEUE; then delete from Hashmap and notify
-			// 	if jobStatus.Status != "IN_QUEUE" {
-			// 		// Notify status
-			// 		job.Conn.WriteJSON(jobStatus)
-			// 		// Remove from Hashmap
-			// 		delete(postedJobs, id)
-			// 		// Disconnect user
-			// 		job.Conn.Close()
-			// 	}
-			// }
+			for id, job := range postedJobs {
+				// Check the status of job in Runpod
+				err, jobStatus := Utilities.GetJobStatus(id)
+				// Drop the item from the map if not TERMINATED
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+				// If status is not TERMINATED; then delete from Hashmap and notify
+				if jobStatus.Status == "COMPLETED" || jobStatus.Status == "TERMINATED" || jobStatus.Status == "CANCELLED" || jobStatus.Status == "FAILED" {
+					// Notify status
+					job.Conn.WriteJSON(jobStatus)
+					// Remove from Hashmap
+					delete(postedJobs, id)
+					// Disconnect User
+					job.Conn.Close()
+				} else {
+					// Post Job Status to connection
+					job.Conn.WriteJSON(jobStatus)
+				}
+			}
 		}
 	}()
 	// Define Web Server Routes
